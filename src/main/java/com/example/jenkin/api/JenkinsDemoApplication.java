a3dd65a379f8f653fef13e52d1d10613157ee9ff
vimalraj-tecdata <vimalraj@tecdata.co.in>
2024-08-09 12:59:19 +0530

    first commit

---
+++ src/main/java/com/example/jenkin/api/JenkinsDemoApplication.java
@@ -0,0 +1,25 @@
+package com.example.jenkin.api;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import jakarta.annotation.PostConstruct;
+
+@SpringBootApplication
+public class JenkinsDemoApplication {
+
+	public static Logger logger = LoggerFactory.getLogger(JenkinsDemoApplication.class);
+
+	@PostConstruct
+	public void in() {
+		logger.info("Application Started....!");
+	}
+
+	public static void main(String[] args) {
+		logger.info("Application Executed....!");
+		SpringApplication.run(JenkinsDemoApplication.class, args);
+	}
+
+}
